f0e5957e4bbf219aaf5a963d5ef1243124ce5cca
Jasan Hernandez <jasan.hernandez@dd3tech.mx>
2022-11-08 10:27:37 -0600

    [add] personalized greeting

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,5 +1,6 @@
 import { Link } from 'react-router-dom'
 import { LearnPaths } from '@/routes/paths'
+import { useCallback } from 'react'
 
 const topicList = [
     {
@@ -8,22 +9,34 @@ const topicList = [
     },
     {
         name: 'Present Simple',
-        topic: 'present'
+        topic: 'simple-present'
     },
     {
-        name: 'Future Simple aux(will)',
-        topic: 'future-simple'
+        name: 'Simple Future aux(will)',
+        topic: 'simple-future'
     },
     {
-        name: 'Future Simple aux(going to)',
+        name: 'Future aux(going to)',
         topic: 'future'
     },
 ]
 
+const greetings = {
+    morning: 'Hi, Good morning 🌅 guy!',
+    afternoon: 'Hi, Good evening ☀️ guy!',
+    night: 'Hi, Good night 🌃 guy!'
+}
+
 function Main() {
+    const getGreeting = useCallback(() => {
+        const hours = new Date().getHours()
+        if (hours < 12) return greetings.morning
+        if (hours >= 12 && hours < 19) return greetings.afternoon
+        return greetings.night
+    }, [])
     return (
         <>
-            <h2 className='text-xl font-bold'>Hello {'{{ USER_NAME }}'}</h2>
+            <h2 className='text-xl font-bold'>{getGreeting()}</h2>
             <p className='font-bold mt-6'>List of Topics:</p>
             <ul className='my-3 pl-3 grid gap-2'>
                 {topicList.map(({ topic, name }, index) => (
